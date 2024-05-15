e30b996c1fb261f54de5bbfaa20139959f388d90
Srinath Kailasa <srinathkailasa@gmail.com>
2024-05-15 21:31:39 +0100

    Add double precision implementation for neon

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use std::arch::aarch64::{float32x4_t, float32x4x2_t};
+use std::arch::aarch64::{float32x4_t, float32x4x2_t, float64x2_t};
 
-use rlst::{c32, RlstScalar};
+use rlst::{c32, c64, RlstScalar};
 
 use coe;
-use pulp::{aarch64::Neon, aarch64::NeonFcma, f32x4, Simd};
+use pulp::{aarch64::Neon, aarch64::NeonFcma, f32x4, f64x2, Simd};
 
 fn slice_to_array<T>(slice: &[T]) -> &[T; 64] {
     assert_eq!(slice.len(), 64, "The slice must have exactly 64 elements.");
@@ -129,27 +129,27 @@ pub struct ComplexMul8x8NeonFcma32<'a> {
 
 pub struct ComplexMul8x8NeonFcma64<'a> {
     pub simd: NeonFcma,
-    pub alpha: f32,
-    pub matrix: &'a [c32; 64],
-    pub vector: &'a [c32; 8],
-    pub result: &'a mut [c32; 8],
+    pub alpha: f64,
+    pub matrix: &'a [c64; 64],
+    pub vector: &'a [c64; 8],
+    pub result: &'a mut [c64; 8],
 }
 
-pub struct ComplexMul8x8Avx232<'a> {
-    pub simd: NeonFcma,
-    pub alpha: f32,
-    pub matrix: &'a [c32; 64],
-    pub vector: &'a [c32; 8],
-    pub result: &'a mut [c32; 8],
-}
+// pub struct ComplexMul8x8Avx232<'a> {
+//     pub simd: NeonFcma,
+//     pub alpha: f32,
+//     pub matrix: &'a [c32; 64],
+//     pub vector: &'a [c32; 8],
+//     pub result: &'a mut [c32; 8],
+// }
 
-pub struct ComplexMul8x8Avx264<'a> {
-    pub simd: NeonFcma,
-    pub alpha: f32,
-    pub matrix: &'a [c32; 64],
-    pub vector: &'a [c32; 8],
-    pub result: &'a mut [c32; 8],
-}
+// pub struct ComplexMul8x8Avx264<'a> {
+//     pub simd: NeonFcma,
+//     pub alpha: f32,
+//     pub matrix: &'a [c32; 64],
+//     pub vector: &'a [c32; 8],
+//     pub result: &'a mut [c32; 8],
+// }
 
 impl pulp::NullaryFnOnce for ComplexMul4x4Neon32<'_> {
     type Output = ();
@@ -334,6 +334,143 @@ impl pulp::NullaryFnOnce for ComplexMul8x8NeonFcma32<'_> {
     }
 }
 
+impl pulp::NullaryFnOnce for ComplexMul8x8NeonFcma64<'_> {
+    type Output = ();
+
+    #[inline(always)]
+    fn call(self) -> Self::Output {
+        let Self {
+            simd,
+            alpha,
+            matrix,
+            vector,
+            result,
+        } = self;
+
+        let mut a1 = f64x2(0., 0.);
+        let mut a2 = f64x2(0., 0.);
+        let mut a3 = f64x2(0., 0.);
+        let mut a4 = f64x2(0., 0.);
+        let mut a5 = f64x2(0., 0.);
+        let mut a6 = f64x2(0., 0.);
+        let mut a7 = f64x2(0., 0.);
+        let mut a8 = f64x2(0., 0.);
+
+        let (matrix, _) = pulp::as_arrays::<8, _>(matrix);
+        let [v1, v2, v3, v4, v5, v6, v7, v8]: [f64x2; 8] = pulp::cast(*vector);
+
+        // Unroll loop
+        let [m1, m2, m3, m4, m5, m6, m7, m8]: [f64x2; 8] = pulp::cast(*&matrix[0]);
+        a1 = simd.c64s_mul_add_e(m1, v1, a1);
+        a2 = simd.c64s_mul_add_e(m2, v2, a2);
+        a3 = simd.c64s_mul_add_e(m3, v3, a3);
+        a4 = simd.c64s_mul_add_e(m4, v4, a4);
+        a5 = simd.c64s_mul_add_e(m5, v5, a5);
+        a6 = simd.c64s_mul_add_e(m6, v6, a6);
+        a7 = simd.c64s_mul_add_e(m7, v7, a7);
+        a8 = simd.c64s_mul_add_e(m8, v8, a8);
+
+        let [m1, m2, m3, m4, m5, m6, m7, m8]: [f64x2; 8] = pulp::cast(*&matrix[1]);
+        a1 = simd.c64s_mul_add_e(m1, v1, a1);
+        a2 = simd.c64s_mul_add_e(m2, v2, a2);
+        a3 = simd.c64s_mul_add_e(m3, v3, a3);
+        a4 = simd.c64s_mul_add_e(m4, v4, a4);
+        a5 = simd.c64s_mul_add_e(m5, v5, a5);
+        a6 = simd.c64s_mul_add_e(m6, v6, a6);
+        a7 = simd.c64s_mul_add_e(m7, v7, a7);
+        a8 = simd.c64s_mul_add_e(m8, v8, a8);
+
+        let [m1, m2, m3, m4, m5, m6, m7, m8]: [f64x2; 8] = pulp::cast(*&matrix[2]);
+        a1 = simd.c64s_mul_add_e(m1, v1, a1);
+        a2 = simd.c64s_mul_add_e(m2, v2, a2);
+        a3 = simd.c64s_mul_add_e(m3, v3, a3);
+        a4 = simd.c64s_mul_add_e(m4, v4, a4);
+        a5 = simd.c64s_mul_add_e(m5, v5, a5);
+        a6 = simd.c64s_mul_add_e(m6, v6, a6);
+        a7 = simd.c64s_mul_add_e(m7, v7, a7);
+        a8 = simd.c64s_mul_add_e(m8, v8, a8);
+
+        let [m1, m2, m3, m4, m5, m6, m7, m8]: [f64x2; 8] = pulp::cast(*&matrix[3]);
+        a1 = simd.c64s_mul_add_e(m1, v1, a1);
+        a2 = simd.c64s_mul_add_e(m2, v2, a2);
+        a3 = simd.c64s_mul_add_e(m3, v3, a3);
+        a4 = simd.c64s_mul_add_e(m4, v4, a4);
+        a5 = simd.c64s_mul_add_e(m5, v5, a5);
+        a6 = simd.c64s_mul_add_e(m6, v6, a6);
+        a7 = simd.c64s_mul_add_e(m7, v7, a7);
+        a8 = simd.c64s_mul_add_e(m8, v8, a8);
+
+        let [m1, m2, m3, m4, m5, m6, m7, m8]: [f64x2; 8] = pulp::cast(*&matrix[4]);
+        a1 = simd.c64s_mul_add_e(m1, v1, a1);
+        a2 = simd.c64s_mul_add_e(m2, v2, a2);
+        a3 = simd.c64s_mul_add_e(m3, v3, a3);
+        a4 = simd.c64s_mul_add_e(m4, v4, a4);
+        a5 = simd.c64s_mul_add_e(m5, v5, a5);
+        a6 = simd.c64s_mul_add_e(m6, v6, a6);
+        a7 = simd.c64s_mul_add_e(m7, v7, a7);
+        a8 = simd.c64s_mul_add_e(m8, v8, a8);
+
+        let [m1, m2, m3, m4, m5, m6, m7, m8]: [f64x2; 8] = pulp::cast(*&matrix[5]);
+        a1 = simd.c64s_mul_add_e(m1, v1, a1);
+        a2 = simd.c64s_mul_add_e(m2, v2, a2);
+        a3 = simd.c64s_mul_add_e(m3, v3, a3);
+        a4 = simd.c64s_mul_add_e(m4, v4, a4);
+        a5 = simd.c64s_mul_add_e(m5, v5, a5);
+        a6 = simd.c64s_mul_add_e(m6, v6, a6);
+        a7 = simd.c64s_mul_add_e(m7, v7, a7);
+        a8 = simd.c64s_mul_add_e(m8, v8, a8);
+
+        let [m1, m2, m3, m4, m5, m6, m7, m8]: [f64x2; 8] = pulp::cast(*&matrix[6]);
+        a1 = simd.c64s_mul_add_e(m1, v1, a1);
+        a2 = simd.c64s_mul_add_e(m2, v2, a2);
+        a3 = simd.c64s_mul_add_e(m3, v3, a3);
+        a4 = simd.c64s_mul_add_e(m4, v4, a4);
+        a5 = simd.c64s_mul_add_e(m5, v5, a5);
+        a6 = simd.c64s_mul_add_e(m6, v6, a6);
+        a7 = simd.c64s_mul_add_e(m7, v7, a7);
+        a8 = simd.c64s_mul_add_e(m8, v8, a8);
+
+        let [m1, m2, m3, m4, m5, m6, m7, m8]: [f64x2; 8] = pulp::cast(*&matrix[7]);
+        a1 = simd.c64s_mul_add_e(m1, v1, a1);
+        a2 = simd.c64s_mul_add_e(m2, v2, a2);
+        a3 = simd.c64s_mul_add_e(m3, v3, a3);
+        a4 = simd.c64s_mul_add_e(m4, v4, a4);
+        a5 = simd.c64s_mul_add_e(m5, v5, a5);
+        a6 = simd.c64s_mul_add_e(m6, v6, a6);
+        a7 = simd.c64s_mul_add_e(m7, v7, a7);
+        a8 = simd.c64s_mul_add_e(m8, v8, a8);
+
+        let a1: float64x2_t = unsafe { std::mem::transmute(a1) };
+        let a2: float64x2_t = unsafe { std::mem::transmute(a2) };
+        let a3: float64x2_t = unsafe { std::mem::transmute(a3) };
+        let a4: float64x2_t = unsafe { std::mem::transmute(a4) };
+        let a5: float64x2_t = unsafe { std::mem::transmute(a5) };
+        let a6: float64x2_t = unsafe { std::mem::transmute(a6) };
+        let a7: float64x2_t = unsafe { std::mem::transmute(a7) };
+        let a8: float64x2_t = unsafe { std::mem::transmute(a8) };
+
+        let a1 = simd.neon.vmulq_n_f64(a1, alpha);
+        let a2 = simd.neon.vmulq_n_f64(a2, alpha);
+        let a3 = simd.neon.vmulq_n_f64(a3, alpha);
+        let a4 = simd.neon.vmulq_n_f64(a4, alpha);
+        let a5 = simd.neon.vmulq_n_f64(a5, alpha);
+        let a6 = simd.neon.vmulq_n_f64(a6, alpha);
+        let a7 = simd.neon.vmulq_n_f64(a7, alpha);
+        let a8 = simd.neon.vmulq_n_f64(a8, alpha);
+
+        let ptr = result.as_ptr() as *mut f64;
+        unsafe { simd.neon.vst1q_f64(ptr, a1) };
+        unsafe { simd.neon.vst1q_f64(ptr.add(2), a2) };
+        unsafe { simd.neon.vst1q_f64(ptr.add(4), a3) };
+        unsafe { simd.neon.vst1q_f64(ptr.add(6), a4) };
+        unsafe { simd.neon.vst1q_f64(ptr.add(8), a5) };
+        unsafe { simd.neon.vst1q_f64(ptr.add(10), a6) };
+        unsafe { simd.neon.vst1q_f64(ptr.add(12), a7) };
+        unsafe { simd.neon.vst1q_f64(ptr.add(14), a8) };
+    }
+}
+
+
 #[cfg(test)]
 mod test {
 
@@ -341,7 +478,52 @@ mod test {
     use num_traits::*;
 
     #[test]
-    fn test_8x8() {
+    fn test_8x8_f64() {
+        let mut expected = [c64::zero(); 8];
+
+        let mut matrix = [c64::zero(); 64];
+        let mut vector = [c64::zero(); 8];
+        let alpha = c64::one() * 10.;
+
+        for i in 0..8 {
+            let num = (i + 1) as f64;
+            vector[i] = c64::new(num, num);
+            for j in 0..8 {
+                matrix[i * 8 + j] = c64::new((i + 1) as f64, (j + 1) as f64);
+            }
+        }
+
+        matvec8x8_col_major(&matrix, &vector, &mut expected, alpha);
+
+        let mut matrix = [c64::zero(); 64];
+        let mut vector = [c64::zero(); 8];
+        let mut result = [c64::zero(); 8];
+
+        for i in 0..8 {
+            let num = (i + 1) as f64;
+            vector[i] = c64::new(num, num);
+            for j in 0..8 {
+                matrix[i*8 + j] = c64::new((i + 1) as f64, (j + 1) as f64);
+            }
+        }
+
+        let simd = NeonFcma::try_new().unwrap();
+        simd.vectorize(ComplexMul8x8NeonFcma64 {
+            simd,
+            alpha: alpha.re(),
+            matrix: &matrix,
+            vector: &vector,
+            result: &mut result,
+        });
+
+        expected.iter().zip(result).for_each(|(e, r)| {
+            println!("e {:?} r {:?}", e, r);
+            assert!((e - r).abs() < 1e-10)
+        });
+    }
+
+    #[test]
+    fn test_8x8_f32() {
         let mut expected = [c32::zero(); 8];
 
         let mut matrix = [c32::zero(); 64];
